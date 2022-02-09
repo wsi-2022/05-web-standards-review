9b0fbaa2b530c985cda2f0c9ed0b5afc511fd05d
Karl Stolley <karl.stolley@gmail.com>
2022-02-09 16:09:33 -0600

    Enable form submission when username has value

--- site.js
+++ site.js
@@ -1,6 +1,18 @@
 'use strict';
 
 var ghForm = document.querySelector('#github-form');
+var ghUsername = ghForm.querySelector('#username');
+var ghSubmit = ghForm.querySelector('#submit');
+
+ghUsername.addEventListener('input', function(event) {
+  if (this.value !== '') {
+    ghSubmit.removeAttribute('disabled');
+    ghSubmit.setAttribute('aria-disabled', 'false');
+  } else {
+    ghSubmit.setAttribute('disabled', 'disabled');
+    ghSubmit.setAttribute('aria-disabled', 'true');
+  }
+});
 
 ghForm.addEventListener('submit', function(event) {
   event.preventDefault();
